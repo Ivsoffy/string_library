0a1bb9479dba988747e8a47ee117f44a940b5c58
Otheym Alpha <otheymal@student.21-school.ru>
2023-01-29 16:58:10 +0000

    Replace s21_string.h

--- src/s21_string.h
+++ src/s21_string.h
@@ -1,78 +1,65 @@
 #ifndef SRC_S21_STRING_H_
 #define SRC_S21_STRING_H_
 
-/*	Выполняет поиск первого вхождения символа c (беззнаковый тип)
-в первых n байтах строки, на которую указывает аргумент str. */
+#define S21_NULL (void *)0
+#define s21_size_t unsigned long int
+
+#include <stdio.h>
+// убрать потом
+//#include <string.h>
+//#include <stdlib.h>
+//#include <stdarg.h>
+
 void *s21_memchr(const void *str, int c, s21_size_t n);
-
-//  Сравнивает первые n байтов str1 и str2.
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
-
-//  Копирует n символов из src в dest.
 void *s21_memcpy(void *dest, const void *src, s21_size_t n);
-
-//  Еще одна функция для копирования n символов из src в dest.
-void *s21_memmove(void *dest, const void *src, s21_size_t n);  
-
-/*  Копирует символ c (беззнаковый тип) в первые n символов строки,
-на которую указывает аргумент str. */
+void *s21_memmove(void *dest, const void *src, s21_size_t n);
 void *s21_memset(void *str, int c, s21_size_t n);
-
-/* Добавляет строку, на которую указывает src, в конец строки,
-на которую указывает dest. */
 char *s21_strcat(char *dest, const char *src);
-
-/* Добавляет строку, на которую указывает src, в конец строки,
-на которую указывает dest, длиной до n символов. */
 char *s21_strncat(char *dest, const char *src, s21_size_t n);
-
-/* Выполняет поиск первого вхождения символа c (беззнаковый тип)
-в строке, на которую указывает аргумент str. */
 char *s21_strchr(const char *str, int c);
-
-/* Сравнивает строку, на которую указывает str1,
-со строкой, на которую указывает str2. */
 int s21_strcmp(const char *str1, const char *str2);
-
-// Сравнивает не более первых n байтов str1 и str2.
 int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
-
-// Копирует строку, на которую указывает src, в dest.
 char *s21_strcpy(char *dest, const char *src);
-
-// Копирует до n символов из строки, на которую указывает src, в dest.
 char *s21_strncpy(char *dest, const char *src, s21_size_t n);
-
-/* Вычисляет длину начального сегмента str1, который полностью
-состоит из символов, не входящих в str2. */
-size_t s21_strcspn(const char *str1, const char *str2);
-
-/*  Выполняет поиск во внутреннем массиве номера ошибки errnum и возвращает
-указатель на строку с сообщением об ошибке. Нужно объявить макросы, содержащие
-массивы сообщений об ошибке для операционных систем mac и linux.
-Описания ошибок есть в оригинальной библиотеке. 
-Проверка текущей ОС осуществляется с помощью директив. */
+s21_size_t s21_strcspn(const char *str1, const char *str2);
 char *s21_strerror(int errnum);
-
-//  Вычисляет длину строки str, не включая завершающий нулевой символ.
-size_t s21_strlen(const char *str);
-
-/* Находит первый символ в строке str1, который соответствует любому символу,
-указанному в str2. */
+s21_size_t s21_strlen(const char *str);
 char *s21_strpbrk(const char *str1, const char *str2);
-
-/* Выполняет поиск последнего вхождения символа c (беззнаковый тип) в строке,
-на которую указывает аргумент str. */
 char *s21_strrchr(const char *str, int c);
-
-//  Вычисляет длину начального сегмента str1, который полностью состоит из символов str2.
-size_t s21_strspn(const char *str1, const char *str2);
-
-/* Находит первое вхождение всей строки needle
-(не включая завершающий нулевой символ), которая появляется в строке haystack. */
+s21_size_t s21_strspn(const char *str1, const char *str2);
 char *s21_strstr(const char *haystack, const char *needle);
-
-//  Разбивает строку str на ряд токенов, разделенных delim.
 char *s21_strtok(char *str, const char *delim);
+
+// sprintf
+typedef struct option {
+  int work;
+  int minus;
+  int plus;
+  int space;
+  int width;
+  int width_value;
+  int accuracy;
+  int accuracy_value;
+  int l;
+  int h;
+} flags;
+
+int s21_sprintf(char *str, const char *format, ...);
+void update_opt(flags *opt);
+void check_opt(flags *opt, int *index, const char *format);
+void print_specificator(const char *format, int index, char *str, va_list args,
+                        int *n, flags *opt);
+int take_int(int *index, const char *format);
+void print_d(char *str, va_list args, int *n, flags *opt);
+long long int len_of_int(long long int x);
+void paste_int(char *str_part, int *i, long long int len,
+               long long int argument);
+void print_int(char *str, int *n, flags *opt, long long int argument_ll);
+void print_u(char *str, va_list args, int *n, flags *opt);
+void print_unsigned_int(char *str, int *n, flags *opt,
+                        unsigned long long int argument_ll);
+void print_c(char *str, va_list args, int *n, flags *opt);
+void print_s(char *str, va_list args, int *n, flags *opt);
 
 #endif  // SRC_S21_STRING_H_
