f44d578bfba7783df4a0e5d466ef197b720aaec6
Vileplume Metis <vileplme@cl-a8.nsk.21-school.ru>
2023-01-29 15:27:41 +0700

    1

--- src/s21_string.h
+++ src/s21_string.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 // убрать потом
 #include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #define S21_NULL (void *)0
 #define s21_size_t unsigned long int
 
@@ -9,3 +11,29 @@ int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
 char *s21_strchr(const char *str, int c);
 char *s21_strcat(char *dest, const char *src);
 char *s21_strncat(char *dest, const char *src, s21_size_t n);
+
+
+// sprintf
+typedef struct option {
+    int work;
+    int minus;
+    int plus;
+    int space;
+    int width;
+    int width_value;
+    int accuracy;
+    int accuracy_value;
+    int l;
+    int h;   
+}flags;
+
+
+int s21_sprintf(char* str, const char* format, ...);
+void update_opt(flags *opt);
+void check_opt(flags *opt, int *index, const char *format);
+void print_specificator(const char *format, int index, char *str, va_list args,
+                        int *n, flags *opt);
+int take_int(int *index, const char *format);
+void print_d(char *str, va_list args, int *n, flags *opt);
+long long int len_of_int(long long int x);
+void paste_int(char *str_part, int *i, long long int len, long long int argument);
---
+++ src/sprintf.c
@@ -0,0 +1,196 @@
+#include "s21_string.h"
+
+
+
+int main() {
+  char *str = (char *)calloc(1,1000);
+  s21_sprintf(str,"bebra%ldbebra%hd", 9223372036854775111, 0);
+  printf("%s", str);
+  printf("\nbebra%ldbebra%hd", 9223372036854775111, 0);
+  
+
+}
+
+int s21_sprintf(char* str, const char* format, ...) {
+    int rv_count = 0;
+    // поменять на s21_strlen
+    int length = strlen(format);
+    va_list(args);
+    va_start(args, format);
+    flags opt;
+    for (int index = 0; index < length; index++) {
+      if (format[index] == '%') {
+        update_opt(&opt);
+        check_opt(&opt, &index, format);
+        print_specificator(format, index, str, args, &rv_count, &opt);
+      } else {
+        str[rv_count] = format[index];
+        rv_count++;
+      }
+    }
+    str[rv_count] = '\0';
+    va_end(args);
+    return rv_count;
+}
+
+void update_opt(flags *opt) { 
+    opt->work = 1;
+    opt -> minus = 0;
+    opt -> plus = 0;
+    opt -> space = 0;
+    opt -> width = 0;
+    opt -> width_value = 0;
+    opt -> accuracy = 0;
+    opt -> accuracy_value = 0;
+    opt -> l = 0;
+    opt -> h = 0;
+}
+
+void check_opt(flags *opt, int *index, const char *format) {
+  while (opt -> work != 0) {
+    *index += 1;
+    if (format[*index] == '-') {
+      opt -> minus = 1;
+    } else if (format[*index] == '+') {
+      opt ->plus = 1;
+    } else if (format[*index] == ' ') {
+      opt ->space = 1;
+    } else if (format[*index] == '.') {
+      opt ->accuracy = 1;
+      *index += 1;
+      opt -> accuracy_value = take_int(index, format);
+    } else if (format[*index] >= '0' && format[*index] <= '9') {
+      opt->width = 1;
+      opt->width_value = take_int(index, format);
+   
+    } else if (format[*index] == 'h') {
+      opt -> h = 1;
+    } else if (format[*index] == 'l') {
+      opt -> l = 1;
+      if (format[*index + 1] == 'l') {
+        opt -> l = 2;
+        *index += 1;
+      }
+    } else {
+        opt -> work = 0;
+    }
+  }
+}
+
+int take_int(int *index, const char* format) {
+  int start = *index;
+  int end = *index - 1;
+  while (format[*index] >= '0' && format[*index] <= '9') {
+    end++;
+    *index += 1;
+  }
+  *index -= 1;
+  int number = 0;
+  int ten = 1;
+  while (start <= end) {
+    number += (format[end] - '0') * ten;
+    ten *= 10;
+    end--;
+    if (number < 0) {
+      break;
+    }
+  }
+  return number;
+}
+
+void print_specificator(const char *format, int index, char *str, va_list args, int *n, flags *opt) {
+  if (format[index] == 'd') {
+    print_d(str, args, n, opt);
+  } else if (format[index] == 'i') {
+  } else if (format[index] == 'f') {
+  } else if (format[index] == 'c') {
+  } else if (format[index] == 's') {
+  } else if (format[index] == 'u') {
+  } else if (format[index] == '%') {
+  }
+}
+
+void print_d(char *str, va_list args, int *n, flags *opt) {
+  long long int argument_ll;
+  if (opt-> l == 2) {
+    long long int argument = va_arg(args, long long int);
+    argument_ll = (long long int)argument;
+  } else if (opt -> l == 1) {
+    long int argument = va_arg(args, long int);
+    argument_ll = (long long int)argument;
+  } else if (opt->h == 1) {
+    short int argument = va_arg(args, int);
+    argument_ll = (long long int)argument;
+  } else {
+    int argument = va_arg(args, int);
+    argument_ll = argument;
+  }
+  
+  char str_part[248];
+  int i = 0;
+  long long int len = len_of_int(argument_ll);
+  if ((opt -> width == 1) && (opt -> width_value > len) && (opt -> minus == 0)) {
+    while (i < ((opt->width_value) - len)) {
+      if (i == ((opt->width_value) - len) - 1) {
+        if (opt->plus && argument_ll > 0) {
+          str_part[i] = '+';
+        } else if (argument_ll < 0) {
+          str_part[i] = '-';
+        } else {
+          str_part[i] = ' ';
+        }
+        i++;
+        break;
+      }
+      str_part[i] = ' ';
+      i++;
+    }
+    paste_int(str_part, &i, len, argument_ll);
+    *n += (opt->width_value);
+  } else {
+    if (opt->plus && argument_ll > 0) {
+      str_part[i] = '+';
+      *n += 1;
+      i++;
+    } else if (argument_ll < 0) {
+      str_part[i] = '-';
+      *n += 1;
+      i++;
+    } else if (opt -> space == 1) {
+      str_part[i] = ' ';
+      *n += 1;
+      i++;
+    }
+    paste_int(str_part, &i, len, argument_ll);
+    *n += len;
+  }
+  str_part[i] = '\0';
+  strcat(str, str_part);
+}
+
+void paste_int(char *str_part, int *i, long long int len, long long int argument) {
+  int count = 0;
+  if (argument < 0) {
+    argument = - argument;
+  }
+  int copy_i = *i;
+  while (count <= len - 1) {
+    str_part[copy_i + len - count - 1] = (int)(argument % 10) + '0';
+    argument /= 10;
+    *i += 1;
+    count++;
+  }
+}
+
+long long int len_of_int(long long int x) { 
+  long long int rv = 0;
+  if (x < 0) {
+    x = -x;
+  }
+  while (x / 10 != 0) {
+    x /= 10;
+    rv += 1;
+  }
+  rv += 1;
+  return rv;
+}
