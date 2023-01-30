7a43fe7e67a04b4a630c514eb14838c5c6fb0f70
ivsoffi <ivsoffa444@gmail.com>
2023-01-30 19:30:50 +0700

    hope it works

--- src/s21_string.h
+++ src/s21_string.h
@@ -1,11 +1,52 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#define S21_NULL (void *)0
+#define s21_size_t unsigned long int
+#define DEF_VAL 6
+
 
 char *s21_strcpy(char *dest, const char *src);
 char *s21_strncpy(char *dest, const char *src, size_t n);
 size_t s21_strcspn(const char *str1, const char *str2);
 char *s21_strerror(int errnum);
 size_t s21_strlen(const char *str);
+
+typedef struct option {
+    int work;
+    int minus;
+    int plus;
+    int space;
+    int width;
+    int width_value;
+    int accuracy;
+    int accuracy_value;
+    int l;
+    int h;
+    int L;
+}flags;
+
+
+int s21_sprintf(char* str, const char* format, ...);
+void update_opt(flags *opt);
+void check_opt(flags *opt, int *index, const char *format);
+void print_specificator(const char *format, int index, char *str, va_list args,
+                        int *n, flags *opt);
+int take_int(int *index, const char *format);
+void print_d(char *str, va_list args, int *n, flags *opt);
+long long int len_of_int(long long int x);
+void paste_int(char *str_part, int *i, long long int len, long long int argument);
+void print_int(char *str, int *n, flags *opt, long long int argument_ll);
+void print_u(char *str, va_list args, int *n, flags *opt);
+void print_unsigned_int(char *str, int *n, flags *opt, unsigned long long int argument_ll);
+void print_c(char *str, va_list args, int *n, flags *opt);
+void print_s(char *str, va_list args, int *n, flags *opt);
+void paste_str_part(char *str_part, int *n, char *str);
+void print_f(char *str, va_list args, int *n, flags *opt);
+int print_float(char *str, int *n, flags *opt, long double argument_ll);
+void print_flags(char *str_part, int *i, long long int argument, flags *opt);
+
 
 #if defined(__APPLE__)
 #define ERRORLIST                                                              \
