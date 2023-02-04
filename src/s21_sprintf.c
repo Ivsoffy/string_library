3fd9eabc91e50bcbc2771c399e8cd51fb43bcbf4
Vileplume Metis <vileplme@in-e8.nsk.21-school.ru>
2023-02-04 18:06:08 +0700

    1

---
+++ src/s21_sprintf.c
@@ -0,0 +1,535 @@
+#include "s21_string.h"
+
+// int main(){
+//   char str231[400];
+//   char str232[400];
+//   char str130[400];
+//   // s21_sprintf(str130, "%+15.0f", 120.678);
+//   char *str233 = "test: %+- 14f!\ntest: %+ 10f!\ntest: % +25f!";
+//   double num = -365789.34;
+//   // printf("%ld\n",(int)(round(num)));
+//   sprintf(str231, str232, num, num, num);
+//   s21_sprintf(str232, str233, num, num, num);
+//   // printf("===%s\n", str130);
+//   printf("s21 =%s\nn21 =%s",str232,str231);
+//   return 0;
+// }
+
+void print_f(char *str, va_list args, int *n, flags *opt) {
+  long double argument_ll;
+  double argument = va_arg(args, double);
+  argument_ll = (long double)argument;
+  print_float(str, n, opt, argument_ll);
+}
+
+int s21_sprintf(char* str, const char* format, ...) {
+    int rv_count = 0;
+    int length = strlen(format);
+    va_list(args);
+    va_start(args, format);
+    flags opt;
+    for (int index = 0; index < length; index++) {
+      if (format[index] == '%') {
+        update_opt(&opt);
+        check_opt(&opt, &index, format);
+        print_specificator(format, index, str, args, &rv_count, &opt);
+      } else {
+        str[rv_count] = format[index];
+        rv_count++;
+      }
+    }
+    str[rv_count] = '\0';
+    va_end(args);
+    return rv_count;
+}
+
+void update_opt(flags *opt) { 
+    opt->work = 1;
+    opt -> minus = 0;
+    opt -> plus = 0;
+    opt -> space = 0;
+    opt -> width = 0;
+    opt -> width_value = 0;
+    opt -> accuracy = 0;
+    opt -> accuracy_value = 0;
+    opt -> l = 0;
+    opt -> h = 0;
+    opt -> flag = 0;
+}
+
+void check_opt(flags *opt, int *index, const char *format) {
+  while (opt -> work != 0) {
+    *index += 1;
+    if (format[*index] == '-') {
+      opt -> minus = 1;
+    } else if (format[*index] == '+') {
+      opt ->plus = 1;
+    } else if (format[*index] == ' ') {
+      opt ->space = 1;
+    } else if (format[*index] == '.') {
+      opt ->accuracy = 1;
+      *index += 1;
+      opt -> accuracy_value = take_int(index, format);
+    } else if (format[*index] >= '0' && format[*index] <= '9') {
+      opt->width = 1;
+      opt->width_value = take_int(index, format);
+   
+    } else if (format[*index] == 'h') {
+      opt -> h = 1;
+    } else if (format[*index] == 'l') {
+      opt -> l = 1;
+      if (format[*index + 1] == 'l') {
+        opt -> l = 2;
+        *index += 1;
+      }
+    } else {
+        opt -> work = 0;
+    }
+  }
+}
+
+int take_int(int *index, const char* format) {
+  int start = *index;
+  int end = *index - 1;
+  while (format[*index] >= '0' && format[*index] <= '9') {
+    end++;
+    *index += 1;
+  }
+  *index -= 1;
+  int number = 0;
+  int ten = 1;
+  while (start <= end) {
+    number += (format[end] - '0') * ten;
+    ten *= 10;
+    end--;
+    if (number < 0) {
+      break;
+    }
+  }
+  return number;
+}
+
+void print_specificator(const char *format, int index, char *str, va_list args, int *n, flags *opt) {
+switch (format[index]) {
+    case 'd':
+        print_d(str, args, n, opt);
+        break;
+    case 'i':
+        print_d(str, args, n, opt);
+        break;
+    case 'f':
+        print_f(str, args, n, opt);
+        break;
+    case 'c':
+        print_c(str, args, n, opt);
+        break;
+    case 's':
+        print_s(str, args, n, opt);
+        break;
+    case 'u':
+        print_u(str, args, n, opt);
+        break;
+    case '%':
+        paste_str_part("%", n, str);
+        break;
+}
+}
+
+void print_d(char *str, va_list args, int *n, flags *opt) {
+  long long int argument_ll;
+  if (opt-> l == 2) {
+    long long int argument = va_arg(args, long long int);
+    argument_ll = (long long int)argument;
+  } else if (opt -> l == 1) {
+    long int argument = va_arg(args, long int);
+    argument_ll = (long long int)argument;
+  } else if (opt->h == 1) {
+    short int argument = va_arg(args, int);
+    argument_ll = (long long int)argument;
+  } else {
+    int argument = va_arg(args, int);
+    argument_ll = (long long int)argument;
+  }  
+  print_int(str, n, opt, argument_ll);
+}
+
+void paste_int(char *str_part, int *i, long long int len, long long int argument, flags *opt) {
+  int count = 0;
+  if (argument < 0) {
+    argument = - argument;
+  }
+  int copy_i = *i;
+  while (count <= len - 1) {
+    if ((opt->flag)&&(count == (len - 1)&&(str_part[copy_i + len - count - 2]=='.'))){
+      opt->flag = 0;
+      argument /= 10;     
+    }else{
+      str_part[copy_i + len - count - 1] = (int)(argument % 10) + '0';
+      argument /= 10;
+      *i += 1;
+      count++;
+    }
+  }
+}
+
+long long int len_of_int(long long int x) { 
+  long long int rv = 0;
+  if (x < 0) {
+    x = -x;
+  }
+  while (x / 10 != 0) {
+    x /= 10;
+    rv += 1;
+  }
+  rv += 1;
+  return rv;
+}
+
+int print_float(char *str, int *n, flags *opt, long double argument_ll){
+  long double first = 0, second = 0;
+  second = modfl(fabsl(argument_ll), &first);
+  int n_pw = opt->accuracy ? opt->accuracy_value : DEF_VAL;
+  int pw = 1; 
+  long int s_digit = 0, f_digit = 0;
+
+  if ((fabsl(second))<0.1) opt->flag = 1;
+  while(pw<=n_pw){
+    s_digit = lroundl(second*pow(10,pw));
+    if (opt->flag) s_digit+=pow(10,pw-1);
+    pw++;
+  }
+  if ((!opt->accuracy_value)&&(opt->accuracy)){
+    f_digit = (long int)(round(argument_ll));
+  }else{
+    f_digit = (long int)first;
+  }
+  long long int len_f = len_of_int(f_digit);  
+  long long int len_s;
+  if ((!(opt->accuracy_value))&&(opt->accuracy)){
+    len_s = 0;
+  }else{
+    len_s = len_of_int(s_digit);
+  }
+
+  int i = 0;
+  long long int len = len_f+len_s+1;
+  if ((!opt->accuracy_value)&&(opt->accuracy)) len--;
+  char str_part[1024] = {'\0'};
+  if ((opt -> width == 1) && (opt -> width_value > len) && (opt -> minus == 0)) { //func
+    while (i < ((opt->width_value) - len)) {
+      if (i == ((opt->width_value) - len) - 1) {
+        if (opt->plus && argument_ll > 0) {
+          str_part[i] = '+';
+        } else if ((argument_ll < 0)||(argument_ll == -0.0)) {
+          str_part[i] = '-';
+        } else {
+          str_part[i] = ' ';
+        }
+        i++;
+        break;
+      }
+      str_part[i] = ' ';
+      i++;
+    }
+    paste_int(str_part, &i,  len_f, f_digit, opt);
+    if (!((!(opt->accuracy_value))&&(opt->accuracy))){
+      strcat(str_part, ".");
+    i++;
+    paste_int(str_part, &i,  len_s, s_digit, opt);
+    }
+  }else if ((opt -> width == 1) && (opt -> width_value > len) && (opt -> minus == 1)){
+    print_flags_d(str_part, &i, argument_ll, opt);
+    paste_int(str_part, &i,  len_f, f_digit, opt);
+    if (!((!(opt->accuracy_value))&&(opt->accuracy))){
+      strcat(str_part, ".");
+    i++;
+    paste_int(str_part, &i,  len_s, s_digit, opt);
+    }
+    while (i < opt -> width_value) {
+        str_part[i] = ' ';
+        i++;
+    }
+    
+  }else {
+    print_flags_d(str_part, &i, argument_ll, opt);
+    paste_int(str_part, &i,  len_f, f_digit, opt);
+    if (!((!(opt->accuracy_value))&&(opt->accuracy))){
+      strcat(str_part, ".");
+    i++;
+    paste_int(str_part, &i,  len_s, s_digit, opt);
+    }
+  }
+  str_part[i] = '\0';
+  paste_str_part(str_part, n, str);
+  return 0;
+}
+
+void print_flags_i(char *str_part, int *i, long long int argument, flags *opt){
+    if (opt->plus && argument > 0) {
+      str_part[*i] = '+';
+      *i+=1;
+    } else if (argument < 0) {
+      str_part[*i] = '-';
+      *i+=1;
+    } else if (opt -> space == 1) {
+      str_part[*i] = ' ';
+      *i+=1;
+    }
+}
+
+void print_flags_d(char *str_part, int *i, long double argument, flags *opt){
+    if (opt->plus && argument > 0) {
+      str_part[*i] = '+';
+      *i+=1;
+    } else if ((argument < 0)||(argument == -0.0)) { //??
+      str_part[*i] = '-';
+      *i+=1;
+    } else if (opt -> space == 1) {
+      str_part[*i] = ' ';
+      *i+=1;
+    }
+}
+
+void print_int(char *str, int *n, flags *opt, long long int argument_ll) {
+    char str_part[1024] = {'\0'};
+  int i = 0;
+  long long int len = len_of_int(argument_ll);
+  if ((opt -> width == 1) && (opt -> width_value > len) && (opt -> minus == 0)) {
+    while (i < ((opt->width_value) - len)) {
+      if (i == ((opt->width_value) - len) - 1) {
+        if (opt->plus && argument_ll > 0) {
+          str_part[i] = '+';
+        } else if (argument_ll < 0) {
+          str_part[i] = '-';
+        } else {
+          str_part[i] = ' ';
+        }
+        i++;
+        break;
+      }
+      str_part[i] = ' ';
+      i++;
+    }
+    paste_int(str_part, &i, len, argument_ll, opt);
+  } else if ((opt -> width == 1) && (opt -> width_value > len) && (opt -> minus == 1)) {
+    print_flags_i(str_part, &i, argument_ll, opt);
+    paste_int(str_part, &i, len, argument_ll, opt);
+    while (i < opt -> width_value) {
+        str_part[i] = ' ';
+        i++;
+    }
+  } else {
+    print_flags_i(str_part, &i, argument_ll, opt);
+    paste_int(str_part, &i, len, argument_ll, opt);
+  }
+  str_part[i] = '\0';
+  paste_str_part(str_part, n, str);
+}
+
+void print_u(char *str, va_list args, int *n, flags *opt) {
+    unsigned long long int argument_ll;
+  if (opt-> l == 2) {
+    unsigned long long int argument = va_arg(args, unsigned long long int);
+    argument_ll = (unsigned long long int)argument;
+  } else if (opt -> l == 1) {
+    unsigned long int argument = va_arg(args, unsigned long int);
+    argument_ll = (unsigned long long int)argument;
+  } else if (opt->h == 1) {
+    unsigned short int argument = va_arg(args, unsigned int);
+    argument_ll = (unsigned long long int)argument;
+  } else {
+    unsigned int argument = va_arg(args, unsigned int);
+    argument_ll = (unsigned long long int)argument;
+  }
+  print_unsigned_int(str, n, opt, argument_ll);
+}
+
+void print_unsigned_int(char *str, int *n, flags *opt, unsigned long long int argument_ll) {
+    char str_part[1024] = {'\0'};
+  int i = 0;
+  long long int len = len_of_int(argument_ll);
+  if ((opt -> width == 1) && (opt -> width_value > len) && (opt -> minus == 0)) {
+    while (i < ((opt->width_value) - len)) {
+      if (i == ((opt->width_value) - len) - 1) {
+        if (opt->plus && argument_ll > 0) {
+          str_part[i] = '+';
+        } else {
+          str_part[i] = ' ';
+        }
+        i++;
+        break;
+      }
+      str_part[i] = ' ';
+      i++;
+    }
+    paste_int(str_part, &i, len, argument_ll, opt);
+  } else if ((opt -> width == 1) && (opt -> width_value > len) && (opt -> minus == 1)) {
+    print_flags_i(str_part, &i, argument_ll, opt);
+    paste_int(str_part, &i, len, argument_ll, opt);
+    while (i < opt -> width_value) {
+        str_part[i] = ' ';
+        i++;
+    }
+  } else {
+    print_flags_i(str_part, &i, argument_ll, opt);
+    paste_int(str_part, &i, len, argument_ll, opt);
+  }
+  str_part[i] = '\0';
+  paste_str_part(str_part, n, str);
+}
+
+void print_c(char *str, va_list args, int *n, flags *opt) {
+    char argument = va_arg(args, int);
+    char str_part[1024] = {'\0'};
+    int i = 0;
+    if (opt -> width == 1 && opt -> width_value > 1) {
+        if (opt -> minus == 1) {
+            str_part[i] = argument;
+            i++;
+            for (int j = 0; j < (opt -> width_value) - 1; ++j) {
+                str_part[i] = ' ';
+                i++;
+            }
+        } else {
+            for (int j = 0; j < (opt -> width_value) - 1; ++j) {
+                str_part[i] = ' ';
+                i++;
+            }
+            str_part[i] = argument;
+            i++;
+        } 
+    } else {
+      str_part[i] = argument;
+      i++;
+    }
+    str_part[i] = '\0';
+    paste_str_part(str_part, n, str);
+}
+
+void print_s(char *str, va_list args, int *n, flags *opt) {
+    char *argument = va_arg(args, char *);
+    if (argument == S21_NULL) {
+        argument = "(null)";
+    }
+    char str_part[2048];
+    int len = strlen(argument);
+    if (opt -> accuracy == 1 && opt -> accuracy_value < len) {
+        len = opt -> accuracy_value;
+    }
+    int i = 0;
+    if (opt -> width == 1 && opt -> width_value > len) {
+        if (opt -> minus == 1) {
+            for (int j = 0; j < len; ++j) {
+                str_part[i] = argument[j];
+                i++;
+            }
+            for (int j = 0; j < (opt -> width_value) - len; ++j) {
+                str_part[i] = ' ';
+                i++;
+            }
+        } else {
+            for (int j = 0; j < (opt -> width_value) - len; ++j) {
+                str_part[i] = ' ';
+                i++;
+            }
+            for (int j = 0; j < len; ++j) {
+                str_part[i] = argument[j];
+                i++;
+            }
+        }
+    } else {
+      for (int j = 0; j < len; ++j) {
+                str_part[i] = argument[j];
+                i++;
+      }
+    }
+    str_part[i] = '\0';
+    paste_str_part(str_part, n, str);
+}
+
+void paste_str_part(char *str_part, int *n, char *str) {
+  int len_part = strlen(str_part);
+  for (int j = 0; j < len_part; ++j) {
+    str[*n] = str_part[j];
+    *n += 1;
+  }
+}
+
+void* s21_trim(const char* src, const char* trim_chars) {
+    char* rv = NULL;
+    if (src != NULL && trim_chars != NULL) {
+        char* start_ptr = (char*)src;
+        char* end_ptr = (char*)src + strlen(src);
+        while (start_ptr != NULL && strchr(trim_chars, *start_ptr)) {
+          start_ptr++;
+        }
+        while (end_ptr != NULL && strchr(trim_chars, *(end_ptr - 1))) {
+          end_ptr--;
+        }
+        rv = (char*)malloc(end_ptr - start_ptr + 1);
+        if (rv != NULL) {
+            strncpy(rv, start_ptr, end_ptr - start_ptr);
+            *(rv + (end_ptr - start_ptr)) = '\0';
+        }
+    }
+    return rv;
+}
+
+void* s21_to_upper(const char* str) {
+    char* rv = NULL;
+    if (str != NULL) {
+      int len = strlen(str);
+        rv = (char*)malloc((len + 1));
+        if (rv != NULL) {
+            strcpy(rv, str);
+            char *ptr = rv;
+            while (*ptr) {
+                if (*ptr >= 'a' && *ptr <= 'z') {
+                    *ptr -= 32;
+                }
+                ptr++;
+            }
+        }
+    }
+    return rv;
+}
+
+void* s21_to_lower(const char* str) {
+    char* rv = NULL;
+    if (str != NULL) {
+        int len = strlen(str);
+        rv = (char*)malloc((len + 1));
+        if (rv != NULL) {
+            strcpy(rv, str);
+            char *ptr = rv;
+            while (*ptr) {
+                if (*ptr >= 'A' && *ptr <= 'Z') {
+                    *ptr += 32;
+                }
+                ptr++;
+            }
+        }
+    }
+    return rv;
+}
+
+void* s21_insert(const char *src, const char *str, size_t start_index) {
+    char* rv = NULL;
+    if (src != NULL && str != NULL) {
+        int len_src = strlen(src);
+        int len_str = strlen(str);
+        if (len_src >= (int)start_index) {
+          rv = malloc(len_src + len_str + 1);
+        }
+        char *ptr = rv;
+        if (rv) {
+            strncpy(rv, src, start_index);
+            ptr += start_index;
+            strcpy(ptr, str);
+            ptr += len_str;
+            strcpy(ptr, src + start_index);
+        }
+    }
+    return rv;
+}
+
---
+++ src/s21_string.c
@@ -0,0 +1,304 @@
+#include "./s21_string.h"
+
+// otheymal block code
+void *s21_memchr(const void *str, int c, s21_size_t n) {
+  void *rez = S21_NULL;
+  int breakFlag = 0;
+  for (s21_size_t i = 0; i < n && breakFlag == 0; i++) {
+    if (*((char *)str + i) == c) {
+      rez = (char *)str + i;
+      breakFlag = 1;
+    }
+  }
+  return rez;
+}
+
+int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
+  unsigned char *s1 = (unsigned char *)str1;
+  unsigned char *s2 = (unsigned char *)str2;
+  int rez = 0;
+  for (s21_size_t i = 0; i < n; i++) {
+    rez = *s1 - *s2;
+    if (*s1 != *s2)
+      break;
+    s1++;
+    s2++;
+  }
+  return rez;
+}
+
+void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
+  void *startDest = dest;
+  char *ptrdest = dest;
+  const char *ptrsrc = src;
+  while (n--) {
+    *ptrdest = *ptrsrc;
+    ptrdest++;
+    ptrsrc++;
+  }
+  return startDest;
+}
+
+void *s21_memmove(void *dest, const void *src, s21_size_t n) {
+  char *ptrdest = dest;
+  const char *ptrsrc = src;
+  s21_size_t i;
+  for (i = 0; i < n; i++) {
+    ptrdest[i] = ptrsrc[i];
+  }
+  return ptrdest;
+}
+
+void *s21_memset(void *str, int c, s21_size_t n) {
+  char *istr = str;
+  int i = 0;
+  while (n > 0) {
+    istr[i] = c;
+    i++;
+    n--;
+  }
+return istr;
+}
+
+// Vileplme block code
+int s21_strcmp(const char *str1, const char *str2) {
+  unsigned char *str_1 = (unsigned char *)str1;
+  unsigned char *str_2 = (unsigned char *)str2;
+  for (; *str_1 && *str_1 == *str_2; str_1++, str_2++)
+    ;
+  return *str_1 - *str_2;
+}
+
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
+  int rv = 0;
+  char *str_1 = (char *)str1;
+  char *str_2 = (char *)str2;
+  for (s21_size_t i = 0; i < n && *str_1 && *str_1 == *str_2;
+       str_1++, str_2++, i++)
+    ;
+  if (n == 0) {
+    rv = 0;
+  } else {
+    rv = *str_1 - *str_2;
+  }
+  return rv;
+}
+
+char *s21_strchr(const char *str, int c) {
+  int have = 0;
+  while (*str != '\0') {
+    if (*str == (unsigned char)c) {
+      have = 1;
+      break;
+    }
+    str++;
+  }
+  char *rv = (char *)str;
+  if (*str == '\0' && c == '\0') {
+    rv = (char *)str;
+  } else if (have == 0) {
+    rv = S21_NULL;
+  }
+  return rv;
+}
+
+char *s21_strcat(char *dest, const char *src) {
+  char *ptr = dest;
+  while (*ptr) {
+    ptr++;
+  }
+  while (*src != '\0') {
+    *ptr = *src;
+    ptr++;
+    src++;
+  }
+  *ptr = '\0';
+  return dest;
+}
+
+char *s21_strncat(char *dest, const char *src, s21_size_t n) {
+  char *ptr = dest;
+  while (*ptr) {
+    ptr++;
+  }
+  s21_size_t i = 0;
+  while (*src != '\0' && i < n) {
+    *ptr = *src;
+    ptr++;
+    src++;
+    i++;
+  }
+  *ptr = '\0';
+  return dest;
+}
+
+// Errokele block code
+char *s21_strcpy(char *dest, const char *src) {
+  char *cp = dest;
+  while (*src) {
+    *(dest++) = *(src++);
+  }
+  *dest = '\0';
+  return cp;
+}
+
+char *s21_strncpy(char *dest, const char *src, size_t n) {
+  char *cp = dest;
+  int i = 0;
+  while (i < (int)n) {
+    if (*(src)) {
+      *(dest++) = *(src++);
+    } else {
+      *(dest++) = '\0';
+    }
+    i++;
+  }
+  return cp;
+}
+
+size_t s21_strlen(const char *str) {
+  size_t len = 0;
+  while (*(str++)) {
+    len++;
+  }
+  return len;
+}
+
+size_t s21_strcspn(const char *str1, const char *str2) {
+  size_t rez = 0;
+  size_t cnt = 0;
+  int i = 0, j = 0;
+  while ((str1[i]) && ((int)cnt != -1)) {
+    j = 0;
+    while ((str2[j]) && ((int)cnt != -1)) {
+      if (str1[i] == str2[j]) {
+        rez = cnt;
+        cnt = -1;
+      }
+      j++;
+    }
+    if ((int)cnt != -1)
+      cnt++;
+    i++;
+  }
+  rez = s21_strlen(str2) ? rez : s21_strlen(str1);
+  return rez;
+}
+
+char *s21_strerror(int errnum) {
+  char *err[] = ERRORLIST;
+  static char rez[100];
+  int max = (*err[0] == 'S') ? 133 : 106;
+  if (errnum >= 0 && errnum <= max) {
+    s21_strcpy(rez, err[errnum]);
+  } else {
+    s21_sprintf(rez, "%s%d", "Unknown error: ", errnum); // change to s21_spintf
+  }
+  return rez;
+}
+
+// Zasteran block code
+char *s21_strpbrk(const char *str1, const char *str2) {
+  char *result = S21_NULL;
+  int exit_flag = 0;
+  for (int i = 0; str1[i] != '\0' && exit_flag == 0; i++) {
+    for (int j = 0; str2[j] != 0; j++) {
+      if (str1[i] == str2[j]) {
+        result = (char *)str1 + 1;
+        exit_flag = 1;
+        break;
+      }
+    }
+  }
+  return result;
+}
+
+char *s21_strrchr(const char *str, int c) {
+  char *result = S21_NULL;
+  while (*str != '\0') {
+    if (*str == (unsigned char)c) {
+      result = (char *)str;
+    }
+    str++;
+  }
+  return result;
+}
+
+s21_size_t s21_strspn(const char *str1, const char *str2) {
+  s21_size_t result = 0;
+  int exit_flag = 0;
+  const char *ptr1;
+  const char *ptr2;
+
+  for (ptr1 = str1; *ptr1 && exit_flag == 0; ptr1++) {
+    for (ptr2 = str2;; ptr2++) {
+      if (*ptr2 == '\0') {
+        result = (ptr1 - str1);
+        exit_flag = 1;
+        break;
+      } else {
+        if (*ptr1 == *ptr2) {
+          break;
+        }
+      }
+    }
+  }
+  if (exit_flag == 0) {
+    result = (ptr1 - str1);
+  }
+  return result;
+}
+
+char *s21_strstr(const char *haystack, const char *needle) {
+  char *result = S21_NULL;
+  int exit_flag = 0;
+
+  if (*needle == '\0') {
+    result = (char *)haystack;
+    exit_flag = 1;
+  }
+  if (exit_flag == 0) {
+    for (int i = 0; haystack[i] != '\0' && exit_flag == 0; i++) {
+      int temp = i;
+      int j = 0;
+      while (haystack[i++] == needle[j++]) {
+        if (needle[j] == '\0') {
+          result = (char *)&haystack[temp];
+          exit_flag = 1;
+          break;
+        }
+      }
+      i = temp;
+    }
+  }
+  return result;
+}
+
+char *s21_strtok(char *str, const char *delim) {
+  char *result = S21_NULL;
+  // int exit_flag = 0;
+  static char *last;
+  // register int ch;
+
+  if (str == S21_NULL && delim != S21_NULL) {
+    str = last;
+  }
+  if (str != S21_NULL && delim != S21_NULL) {
+    s21_size_t str1 = 0, str2 = 0;
+    for (; str[str1] && str[str2] != delim[str2]; str2++) {
+      if (delim[str2] == '\0') {
+        str1++;
+        str2 = -1;
+      }
+    }
+    if (str[str1] == '\0' || str[str1 + 1] == '\0') {
+      str[str1] = '\0';
+      last = S21_NULL;
+    } else {
+      str[str1] = '\0';
+      last = (char *)&str[str1 + 1];
+    }
+    result = str;
+  }
+  return result;
+}
