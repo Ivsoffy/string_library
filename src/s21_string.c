35a453edce72ec0a14a0163392ebb8074894fdca
Zaster Annette <zasteran@in-i1.nsk.21-school.ru>
2023-02-06 21:41:50 +0700

    fix_all_func

--- src/s21_string.c
+++ src/s21_string.c
@@ -1,18 +1,215 @@
 #include "./s21_string.h"
 
-char *s21_strpbrk(const char *str1, const char *str2) {
-    char *result = S21_NULL;
-    int exit_flag = 0;
-    for (int i = 0; str1[i] != '\0' && exit_flag == 0; i++) {
-        for (int j = 0; str2[j] != 0; j++) {
-            if (str1[i] == str2[j]) {
-                result = (char *)str1 + 1;
-                exit_flag = 1;
-            break;
-            }
-        }
+// otheymal block code
+void *s21_memchr(const void *str, int c, s21_size_t n) {
+  void *rez = S21_NULL;
+  int breakFlag = 0;
+  for (s21_size_t i = 0; i < n && breakFlag == 0; i++) {
+    if (*((char *)str + i) == c) {
+      rez = (char *)str + i;
+      breakFlag = 1;
     }
-    return result;
+  }
+  return rez;
+}
+
+int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
+  unsigned char *s1 = (unsigned char *)str1;
+  unsigned char *s2 = (unsigned char *)str2;
+  int rez = 0;
+  for (s21_size_t i = 0; i < n; i++) {
+    rez = *s1 - *s2;
+    if (*s1 != *s2) break;
+    s1++;
+    s2++;
+  }
+  return rez;
+}
+
+void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
+  void *startDest = dest;
+  char *ptrdest = dest;
+  const char *ptrsrc = src;
+  while (n--) {
+    *ptrdest = *ptrsrc;
+    ptrdest++;
+    ptrsrc++;
+  }
+  return startDest;
+}
+
+void *s21_memmove(void *dest, const void *src, s21_size_t n) {
+  char *ptrdest = dest;
+  const char *ptrsrc = src;
+  s21_size_t i;
+  for (i = 0; i < n; i++) {
+    ptrdest[i] = ptrsrc[i];
+  }
+  return ptrdest;
+}
+
+void *s21_memset(void *str, int c, s21_size_t n) {
+  char *istr = str;
+  int i = 0;
+  while (n > 0) {
+    istr[i] = c;
+    i++;
+    n--;
+  }
+  return istr;
+}
+
+// Vileplme block code
+int s21_strcmp(const char *str1, const char *str2) {
+  unsigned char *str_1 = (unsigned char *)str1;
+  unsigned char *str_2 = (unsigned char *)str2;
+  for (; *str_1 && *str_1 == *str_2; str_1++, str_2++)
+    ;
+  return *str_1 - *str_2;
+}
+
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
+  int rv = 0;
+  char *str_1 = (char *)str1;
+  char *str_2 = (char *)str2;
+  for (s21_size_t i = 0; i < n && *str_1 && *str_1 == *str_2;
+       str_1++, str_2++, i++)
+    ;
+  if (n == 0) {
+    rv = 0;
+  } else {
+    rv = *str_1 - *str_2;
+  }
+  return rv;
+}
+
+char *s21_strchr(const char *str, int c) {
+  int have = 0;
+  while (*str != '\0') {
+    if (*str == (unsigned char)c) {
+      have = 1;
+      break;
+    }
+    str++;
+  }
+  char *rv = (char *)str;
+  if (*str == '\0' && c == '\0') {
+    rv = (char *)str;
+  } else if (have == 0) {
+    rv = S21_NULL;
+  }
+  return rv;
+}
+
+char *s21_strcat(char *dest, const char *src) {
+  char *ptr = dest;
+  while (*ptr) {
+    ptr++;
+  }
+  while (*src != '\0') {
+    *ptr = *src;
+    ptr++;
+    src++;
+  }
+  *ptr = '\0';
+  return dest;
+}
+
+char *s21_strncat(char *dest, const char *src, s21_size_t n) {
+  char *ptr = dest;
+  while (*ptr) {
+    ptr++;
+  }
+  s21_size_t i = 0;
+  while (*src != '\0' && i < n) {
+    *ptr = *src;
+    ptr++;
+    src++;
+    i++;
+  }
+  *ptr = '\0';
+  return dest;
+}
+
+// Errokele block code
+char *s21_strcpy(char *dest, const char *src) {
+  char *cp = dest;
+  while (*src) {
+    *(dest++) = *(src++);
+  }
+  *dest = '\0';
+  return cp;
+}
+
+char *s21_strncpy(char *dest, const char *src, s21_size_t n) {
+  char *cp = dest;
+  int i = 0;
+  while (i < (int)n) {
+    if (*(src)) {
+      *(dest++) = *(src++);
+    } else {
+      *(dest++) = '\0';
+    }
+    i++;
+  }
+  return cp;
+}
+
+s21_size_t s21_strlen(const char *str) {
+  s21_size_t len = 0;
+  while (*(str++)) {
+    len++;
+  }
+  return len;
+}
+
+s21_size_t s21_strcspn(const char *str1, const char *str2) {
+  s21_size_t rez = 0;
+  s21_size_t cnt = 0;
+  int i = 0, j = 0;
+  while ((str1[i]) && ((int)cnt != -1)) {
+    j = 0;
+    while ((str2[j]) && ((int)cnt != -1)) {
+      if (str1[i] == str2[j]) {
+        rez = cnt;
+        cnt = -1;
+      }
+      j++;
+    }
+    if ((int)cnt != -1) cnt++;
+    i++;
+  }
+  rez = s21_strlen(str2) ? rez : s21_strlen(str1);
+  return rez;
+}
+
+char *s21_strerror(int errnum) {
+  char *err[] = ERRORLIST;
+  static char rez[100];
+  int max = (*err[0] == 'S') ? 133 : 106;
+  if (errnum >= 0 && errnum <= max) {
+    s21_strcpy(rez, err[errnum]);
+  } else {
+    s21_sprintf(rez, "%s%d",
+                "Unknown error: ", errnum);  // change to s21_spintf
+  }
+  return rez;
+}
+
+// Zasteran block code
+char *s21_strpbrk(const char *str1, const char *str2) {
+  char *result = S21_NULL;
+  int exit_flag = 0;
+  for (int i = 0; str1[i] != '\0' && exit_flag == 0; i++) {
+    for (int j = 0; str2[j] != 0; j++) {
+      if (str1[i] == str2[j]) {
+        result = (char *)str1 + 1;
+        exit_flag = 1;
+        break;
+      }
+    }
+  }
+  return result;
 }
 
 char *s21_strrchr(const char *str, int c) {
@@ -52,53 +249,135 @@ s21_size_t s21_strspn(const char *str1, const char *str2) {
 }
 
 char *s21_strstr(const char *haystack, const char *needle) {
-    char *result = S21_NULL;
-    int exit_flag = 0;
+  char *result = S21_NULL;
+  int exit_flag = 0;
 
-    if (*needle == '\0') {
-        result = (char *)haystack;
-        exit_flag = 1;
-    }
-    if (exit_flag == 0) {
-        for (int i = 0; haystack[i] != '\0' && exit_flag == 0; i++) {
-            int temp = i;
-            int j = 0;
-            while (haystack[i++] == needle[j++]) {
-                if (needle[j] == '\0') {
-                    result = (char *)&haystack[temp];
-                    exit_flag = 1;
-                    break;
-                }
-            }
-            i = temp;
+  if (*needle == '\0') {
+    result = (char *)haystack;
+    exit_flag = 1;
+  }
+  if (exit_flag == 0) {
+    for (int i = 0; haystack[i] != '\0' && exit_flag == 0; i++) {
+      int temp = i;
+      int j = 0;
+      while (haystack[i++] == needle[j++]) {
+        if (needle[j] == '\0') {
+          result = (char *)&haystack[temp];
+          exit_flag = 1;
+          break;
         }
+      }
+      i = temp;
     }
-    return result;
+  }
+  return result;
 }
 
 char *s21_strtok(char *str, const char *delim) {
-    char *result = S21_NULL;
-    static char *last;
+  char *result = S21_NULL;
+  // int exit_flag = 0;
+  static char *last;
+  // register int ch;
 
-    if (str == S21_NULL && delim != S21_NULL) {
-        str = last;
+  if (str == S21_NULL && delim != S21_NULL) {
+    str = last;
+  }
+  if (str != S21_NULL && delim != S21_NULL) {
+    s21_size_t str1 = 0, str2 = 0;
+    for (; str[str1] && str[str1] != delim[str2]; str2++) {
+      if (delim[str2] == '\0') {
+        str1++;
+        str2 = -1;
+      }
     }
-    if (str != S21_NULL && delim != S21_NULL) {
-        s21_size_t str1 = 0, str2 = 0;
-        for (; str[str1] && str[str1] != delim[str2]; str2++) {
-            if (delim[str2] == '\0') {
-                str1++;
-                str2 = -1;
-            }
-        }
-        if (str[str1] == '\0' || str[str1 + 1] == '\0') {
-                str[str1] = '\0';
-                last = S21_NULL;
-        } else {
-            str[str1] = '\0';
-            last = (char *)&str[str1 + 1];
-        }
-        result = str;
+    if (str[str1] == '\0' || str[str1 + 1] == '\0') {
+      str[str1] = '\0';
+      last = S21_NULL;
+    } else {
+      str[str1] = '\0';
+      last = (char *)&str[str1 + 1];
     }
-    return result;
+    result = str;
+  }
+  return result;
+}
+
+// доп функции
+
+void *s21_trim(const char *src, const char *trim_chars) {
+  char *rv = S21_NULL;
+  if (src != S21_NULL && trim_chars != S21_NULL) {
+    char *start_ptr = (char *)src;
+    char *end_ptr = (char *)src + s21_strlen(src);
+    while (start_ptr != S21_NULL && s21_strchr(trim_chars, *start_ptr)) {
+      start_ptr++;
+    }
+    while (end_ptr != S21_NULL && s21_strchr(trim_chars, *(end_ptr - 1))) {
+      end_ptr--;
+    }
+    rv = (char *)malloc(end_ptr - start_ptr + 1);
+    if (rv != S21_NULL) {
+      s21_strncpy(rv, start_ptr, end_ptr - start_ptr);
+      *(rv + (end_ptr - start_ptr)) = '\0';
+    }
+  }
+  return rv;
+}
+
+void *s21_to_upper(const char *str) {
+  char *rv = S21_NULL;
+  if (str != S21_NULL) {
+    int len = s21_strlen(str);
+    rv = (char *)malloc((len + 1));
+    if (rv != S21_NULL) {
+      s21_strcpy(rv, str);
+      char *ptr = rv;
+      while (*ptr) {
+        if (*ptr >= 'a' && *ptr <= 'z') {
+          *ptr -= 32;
+        }
+        ptr++;
+      }
+    }
+  }
+  return rv;
+}
+
+void *s21_to_lower(const char *str) {
+  char *rv = S21_NULL;
+  if (str != S21_NULL) {
+    int len = s21_strlen(str);
+    rv = (char *)malloc((len + 1));
+    if (rv != S21_NULL) {
+      s21_strcpy(rv, str);
+      char *ptr = rv;
+      while (*ptr) {
+        if (*ptr >= 'A' && *ptr <= 'Z') {
+          *ptr += 32;
+        }
+        ptr++;
+      }
+    }
+  }
+  return rv;
+}
+
+void *s21_insert(const char *src, const char *str, s21_size_t start_index) {
+  char *rv = S21_NULL;
+  if (src != S21_NULL && str != S21_NULL) {
+    int len_src = s21_strlen(src);
+    int len_str = s21_strlen(str);
+    if (len_src >= (int)start_index) {
+      rv = malloc(len_src + len_str + 1);
+    }
+    char *ptr = rv;
+    if (rv) {
+      s21_strncpy(rv, src, start_index);
+      ptr += start_index;
+      s21_strcpy(ptr, str);
+      ptr += len_str;
+      s21_strcpy(ptr, src + start_index);
+    }
+  }
+  return rv;
 }
--- src/s21_string.h
+++ src/s21_string.h
@@ -1,25 +1,198 @@
-#ifndef S21_STRING_H_
-#define S21_STRING_H_
+#ifndef SRC_S21_STRING_H_
+#define SRC_S21_STRING_H_
 
-typedef long unsigned s21_size_t;
+#define S21_NULL (void *)0
+#define s21_size_t unsigned long int
+#define DEF_VAL 6
 
-#define S21_NULL ((void *)0)
+#include <math.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// Находит первый символ в строке str1, который соответствует любому символу, указанному в str2.
+void *s21_memchr(const void *str, int c, s21_size_t n);
+int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
+void *s21_memcpy(void *dest, const void *src, s21_size_t n);
+void *s21_memmove(void *dest, const void *src, s21_size_t n);
+void *s21_memset(void *str, int c, s21_size_t n);
+char *s21_strcat(char *dest, const char *src);
+char *s21_strncat(char *dest, const char *src, s21_size_t n);
+char *s21_strchr(const char *str, int c);
+int s21_strcmp(const char *str1, const char *str2);
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
+char *s21_strcpy(char *dest, const char *src);
+char *s21_strncpy(char *dest, const char *src, s21_size_t n);
+s21_size_t s21_strcspn(const char *str1, const char *str2);
+char *s21_strerror(int errnum);
+s21_size_t s21_strlen(const char *str);
 char *s21_strpbrk(const char *str1, const char *str2);
-
-// Выполняет поиск последнего вхождения символа c (беззнаковый тип) в строке, 
-// на которую указывает аргумент str.
 char *s21_strrchr(const char *str, int c);
-
-// Вычисляет длину начального сегмента str1, который полностью состоит из символов str2.
 s21_size_t s21_strspn(const char *str1, const char *str2);
-
-// Находит первое вхождение всей строки needle (не включая завершающий нулевой символ), 
-// которая появляется в строке haystack.
 char *s21_strstr(const char *haystack, const char *needle);
-
-// Разбивает строку str на ряд токенов, разделенных delim.
 char *s21_strtok(char *str, const char *delim);
 
-#endif  // S21_STRING_H_
+// sprintf
+
+typedef struct option {
+  int work;
+  int minus;
+  int plus;
+  int space;
+  int width;
+  int width_value;
+  int accuracy;
+  int accuracy_value;
+  int l;
+  int h;
+  int L;
+  int flag;
+} flags;
+
+int s21_sprintf(char *str, const char *format, ...);
+void update_opt(flags *opt);
+void check_opt(flags *opt, int *index, const char *format);
+void print_specificator(const char *format, int index, char *str, va_list args,
+                        int *n, flags *opt);
+int take_int(int *index, const char *format);
+void print_d(char *str, va_list args, int *n, flags *opt);
+long long int len_of_int(long long int x);
+void paste_int(char *str_part, int *i, long long int len,
+               long long int argument, flags *opt);
+void print_int(char *str, int *n, flags *opt, long long int argument_ll);
+void print_u(char *str, va_list args, int *n, flags *opt);
+void print_unsigned_int(char *str, int *n, flags *opt,
+                        unsigned long long int argument_ll);
+void print_c(char *str, va_list args, int *n, flags *opt);
+void print_s(char *str, va_list args, int *n, flags *opt);
+void paste_str_part(char *str_part, int *n, char *str);
+void print_f(char *str, va_list args, int *n, flags *opt);
+int print_float(char *str, int *n, flags *opt, long double argument_ll);
+void print_flags_i(char *str_part, int *i, long long int argument, flags *opt);
+void print_flags_d(char *str_part, int *i, long double argument, flags *opt);
+void *s21_trim(const char *src, const char *trim_chars);
+void *s21_to_upper(const char *str);
+void *s21_to_lower(const char *str);
+void *s21_insert(const char *src, const char *str, size_t start_index);
+
+#if defined(__APPLE__)
+#define ERRORLIST                                                              \
+  {                                                                            \
+    "Undefined error: 0", "Operation not permitted",                           \
+        "No such file or directory", "No such process",                        \
+        "Interrupted system call", "Input/output error",                       \
+        "Device not configured", "Argument list too long",                     \
+        "Exec format error", "Bad file descriptor", "No child processes",      \
+        "Resource deadlock avoided", "Cannot allocate memory",                 \
+        "Permission denied", "Bad address", "Block device required",           \
+        "Resource busy", "File exists", "Cross-device link",                   \
+        "Operation not supported by device", "Not a directory",                \
+        "Is a directory", "Invalid argument", "Too many open files in system", \
+        "Too many open files", "Inappropriate ioctl for device",               \
+        "Text file busy", "File too large", "No space left on device",         \
+        "Illegal seek", "Read-only file system", "Too many links",             \
+        "Broken pipe", "Numerical argument out of domain", "Result too large", \
+        "Resource temporarily unavailable", "Operation now in progress",       \
+        "Operation already in progress", "Socket operation on non-socket",     \
+        "Destination address required", "Message too long",                    \
+        "Protocol wrong type for socket", "Protocol not available",            \
+        "Protocol not supported", "Socket type not supported",                 \
+        "Operation not supported", "Protocol family not supported",            \
+        "Address family not supported by protocol family",                     \
+        "Address already in use", "Can't assign requested address",            \
+        "Network is down", "Network is unreachable",                           \
+        "Network dropped connection on reset",                                 \
+        "Software caused connection abort", "Connection reset by peer",        \
+        "No buffer space available", "Socket is already connected",            \
+        "Socket is not connected", "Can't send after socket shutdown",         \
+        "Too many references: can't splice", "Operation timed out",            \
+        "Connection refused", "Too many levels of symbolic links",             \
+        "File name too long", "Host is down", "No route to host",              \
+        "Directory not empty", "Too many processes", "Too many users",         \
+        "Disc quota exceeded", "Stale NFS file handle",                        \
+        "Too many levels of remote in path", "RPC struct is bad",              \
+        "RPC version wrong", "RPC prog. not avail", "Program version wrong",   \
+        "Bad procedure for program", "No locks available",                     \
+        "Function not implemented", "Inappropriate file type or format",       \
+        "Authentication error", "Need authenticator", "Device power is off",   \
+        "Device error", "Value too large to be stored in data type",           \
+        "Bad executable (or shared library)", "Bad CPU type in executable",    \
+        "Shared library version mismatch", "Malformed Mach-o file",            \
+        "Operation canceled", "Identifier removed",                            \
+        "No message of desired type", "Illegal byte sequence",                 \
+        "Attribute not found", "Bad message", "EMULTIHOP (Reserved)",          \
+        "No message available on STREAM", "ENOLINK (Reserved)",                \
+        "No STREAM resources", "Not a STREAM", "Protocol error",               \
+        "STREAM ioctl timeout", "Operation not supported on socket",           \
+        "Policy not found", "State not recoverable", "Previous owner died",    \
+        "Interface output queue is full", "Unknown error"                      \
+  }
+
+#else
+#define ERRORLIST                                                              \
+  {                                                                            \
+    "Success", "Operation not permitted", "No such file or directory",         \
+        "No such process", "Interrupted system call", "Input/output error",    \
+        "No such device or address", "Argument list too long",                 \
+        "Exec format error", "Bad file descriptor", "No child processes",      \
+        "Resource temporarily unavailable", "Cannot allocate memory",          \
+        "Permission denied", "Bad address", "Block device required",           \
+        "Device or resource busy", "File exists", "Invalid cross-device link", \
+        "No such device", "Not a directory", "Is a directory",                 \
+        "Invalid argument", "Too many open files in system",                   \
+        "Too many open files", "Inappropriate ioctl for device",               \
+        "Text file busy", "File too large", "No space left on device",         \
+        "Illegal seek", "Read-only file system", "Too many links",             \
+        "Broken pipe", "Numerical argument out of domain",                     \
+        "Numerical result out of range", "Resource deadlock avoided",          \
+        "File name too long", "No locks available",                            \
+        "Function not implemented", "Directory not empty",                     \
+        "Too many levels of symbolic links", "Unknown error 41",               \
+        "No message of desired type", "Identifier removed",                    \
+        "Channel number out of range", "Level 2 not synchronized",             \
+        "Level 3 halted", "Level 3 reset", "Link number out of range",         \
+        "Protocol driver not attached", "No CSI structure available",          \
+        "Level 2 halted", "Invalid exchange", "Invalid request descriptor",    \
+        "Exchange full", "No anode", "Invalid request code", "Invalid slot",   \
+        "Unknown error 58", "Bad font file format", "Device not a stream",     \
+        "No data available", "Timer expired", "Out of streams resources",      \
+        "Machine is not on the network", "Package not installed",              \
+        "Object is remote", "Link has been severed", "Advertise error",        \
+        "Srmount error", "Communication error on send", "Protocol error",      \
+        "Multihop attempted", "RFS specific error", "Bad message",             \
+        "Value too large for defined data type", "Name not unique on network", \
+        "File descriptor in bad state", "Remote address changed",              \
+        "Can not access a needed shared library",                              \
+        "Accessing a corrupted shared library",                                \
+        ".lib section in a.out corrupted",                                     \
+        "Attempting to link in too many shared libraries",                     \
+        "Cannot exec a shared library directly",                               \
+        "Invalid or incomplete multibyte or wide character",                   \
+        "Interrupted system call should be restarted", "Streams pipe error",   \
+        "Too many users", "Socket operation on non-socket",                    \
+        "Destination address required", "Message too long",                    \
+        "Protocol wrong type for socket", "Protocol not available",            \
+        "Protocol not supported", "Socket type not supported",                 \
+        "Operation not supported", "Protocol family not supported",            \
+        "Address family not supported by protocol", "Address already in use",  \
+        "Cannot assign requested address", "Network is down",                  \
+        "Network is unreachable", "Network dropped connection on reset",       \
+        "Software caused connection abort", "Connection reset by peer",        \
+        "No buffer space available",                                           \
+        "Transport endpoint is already connected",                             \
+        "Transport endpoint is not connected",                                 \
+        "Cannot send after transport endpoint shutdown",                       \
+        "Too many references: cannot splice", "Connection timed out",          \
+        "Connection refused", "Host is down", "No route to host",              \
+        "Operation already in progress", "Operation now in progress",          \
+        "Stale file handle", "Structure needs cleaning",                       \
+        "Not a XENIX named type file", "No XENIX semaphores available",        \
+        "Is a named type file", "Remote I/O error", "Disk quota exceeded",     \
+        "No medium found", "Wrong medium type", "Operation canceled",          \
+        "Required key not available", "Key has expired",                       \
+        "Key has been revoked", "Key was rejected by service", "Owner died",   \
+        "State not recoverable", "Operation not possible due to RF-kill",      \
+        "Memory page has hardware error"                                       \
+  }
+#endif
+
+#endif  // SRC_S21_STRING_H_
